61c922f4fe904cd8d35011b2cad551895d7c3777
shadab057 <mohdshadab4629@gmail.com>
2025-03-28 02:21:57 +0530

    Data Types in javaScript

---
+++ 02Day/output2.js
@@ -0,0 +1,62 @@
+// Data Types
+
+// primitive  7 Data types 
+// string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSide = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+// const bigNumber = 3455646465465446564588n
+
+
+
+// Non primitive Data types ( Reference types )
+// Array, Objects, function
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "Shadab",
+    age: 24,
+}
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof outSide);
+
+
+
+
+
+// undefined    "undefined"
+// Null         "object"
+// boolean      "boolean"
+// number       "number"
+// string       "string"
+// object       "object"
+// function     "function"
+
+
+
+
+
+
+
+
+
+
+
+
+
