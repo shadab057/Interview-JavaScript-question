5186439e51149765076e1d2b0b9c2233dd12754d
shadab057 <mohdshadab4629@gmail.com>
2025-04-03 02:07:42 +0530

    Array Destructing In JavaScript

---
+++ 04Day/output01.js
@@ -0,0 +1,14 @@
+//  key in object
+
+const checkKeyInObject = (key, Object) =>{
+    if(!Object){
+        return false;
+    }
+}
+
+const obj = {name: 'Shadab', age: 24}   // API Data 
+console.log(checkKeyInObject('name',))
+
+
+
+// Array destructuring is a syntax introduced in ES6 (ECMAScript 2015) that lets you unpack values from an array into distinct variables in a single line.
