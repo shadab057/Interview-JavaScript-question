cf8fb6f6468552dd94ac0ce03d87c350268f5529
shadab057 <mohdshadab4629@gmail.com>
2025-04-14 02:31:24 +0530

    How we can read and write file in javascript

---
+++ 06Day/output2.js
@@ -0,0 +1,6 @@
+// How we can read and write file in javascript -
+
+// • Below are the ways to read and write files in javascript  -
+// • Using Active X objects
+// • Using JavaScript Extenstion
+
