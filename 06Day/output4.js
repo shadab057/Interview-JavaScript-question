46dc23ee7f5124c37a2f5ff5c5a9e33d12571604
shadab057 <mohdshadab4629@gmail.com>
2025-04-14 13:33:39 +0530

    Which are the loop structure available in javaScript ?

---
+++ 06Day/output4.js
@@ -0,0 +1,20 @@
+// Which are the loop structure available in javaScript ?
+
+// • Below are the available looping structres in javascript -
+
+// • while 
+let i = 0;
+while (i < 5) {
+  console.log(i); // 0, 1, 2, 3, 4
+  i++;
+}
+// • for
+for (let i = 6; i < 10; i++) {
+    console.log(i); // 6, 7, 8, 9,
+  }
+// • Do-while
+// let i = 0;
+// do {
+//   console.log(i); // 0, 1, 2, 3, 4
+//   i++;
+// } while (i < 5);
