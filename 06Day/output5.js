cb2ac70052569a930fce53d6235146d57321ffa2
shadab057 <mohdshadab4629@gmail.com>
2025-04-14 13:38:50 +0530

    How we can detect the operating system version of the machine using javascript?

---
+++ 06Day/output5.js
@@ -0,0 +1,4 @@
+//  How we can detect the operating system version of the machine using javascript?
+
+// • We can use "appversion" function of "Navigator" object like below -
+// • Navigator.appVersion()
