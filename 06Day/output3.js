9295118c992df8b7edc61b6627d096ecd648dbe1
shadab057 <mohdshadab4629@gmail.com>
2025-04-14 13:25:40 +0530

    How to convert string to integer in javascript

---
+++ 06Day/output3.js
@@ -0,0 +1,8 @@
+// How to convert string to integer in javascript 
+
+// • Using "parseInt()" function we can convert string to Integer.
+
+let str = "123";
+let num = parseInt(str);
+console.log(num);
+console.log(typeof num);
